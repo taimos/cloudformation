856efed411ca800b278bab7bf7d8475e67e3f44f
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2016-08-17 14:47:23 +0200

    add Frankfurt region

--- ecr-clean/build.sh
+++ ecr-clean/build.sh
@@ -17,7 +17,7 @@ cfn-include -t -m ecr-clean.template > dist/ecr-clean.compiled.template
 aws s3 --region eu-central-1 cp dist/ecr-clean.compiled.template s3://taimos-cfn-public/templates/ecr-clean.template
 
 # Regional files
-array=( eu-west-1 )
+array=( eu-west-1 eu-central-1 )
 for i in "${array[@]}"
 do
     aws s3 --region ${i} cp dist/ecr-clean.zip s3://taimos-lambda-public-${i}/ecr-clean-${VERSION}.zip
